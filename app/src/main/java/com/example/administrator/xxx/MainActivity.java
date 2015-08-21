e5b9cb5f54a58bd6203e53a2b26d92efd6bf9189
Paul <anyeccc@gmail.com>
2015-08-21 14:37:34 +0800

    use douban api get book information from isbn number

--- app/src/main/java/com/example/administrator/xxx/MainActivity.java
+++ app/src/main/java/com/example/administrator/xxx/MainActivity.java
@@ -13,15 +13,15 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
-import org.json.JSONException;
-import org.json.JSONObject;
-
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
 import java.net.URL;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 public class MainActivity extends Activity {
     private static final String LOG_TAG = "lllllllllllog";
@@ -58,8 +58,8 @@ public class MainActivity extends Activity {
 
     public void searchText(View view) {
         EditText editText = (EditText) findViewById(R.id.edit_message);
-        String message = editText.getText().toString();
-        Log.d(LOG_TAG, message);
+        String isbn = editText.getText().toString();
+        Log.d(LOG_TAG, isbn);
         ConnectivityManager connMgr = (ConnectivityManager)
                 getSystemService(Context.CONNECTIVITY_SERVICE);
         NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
@@ -67,7 +67,7 @@ public class MainActivity extends Activity {
             // fetch data
             Log.d(LOG_TAG, "fetch data");
             textView.setText("fetching data...");
-            new DownloadWebpageTask().execute(message);
+            new DownloadWebpageTask().execute(isbn);
         } else {
             // display error
             Log.d(LOG_TAG, "No network connection available");
@@ -75,34 +75,37 @@ public class MainActivity extends Activity {
         }
     }
 
-    private class DownloadWebpageTask extends AsyncTask<String, Void, String> {
+    private class DownloadWebpageTask extends AsyncTask<String, Void, Book> {
         @Override
-        protected String doInBackground(String... urls) {
+        protected Book doInBackground(String... isbn) {
 
             // params comes from the execute() call: params[0] is the url.
             try {
-                return downloadUrl(urls[0]);
+                return downloadUrl(isbn[0]);
             } catch (IOException e) {
                 Log.d(LOG_TAG, "Unable to retrieve web page. URL may be invalid.");
-                return "Unable to retrieve web page. URL may be invalid.";
+                return null;
             }
         }
         // onPostExecute displays the results of the AsyncTask.
         @Override
-        protected void onPostExecute(String result) {
-            textView.setText(result);
+        protected void onPostExecute(Book result) {
+            Log.d(LOG_TAG, "rrrrrrrrrrrrrrrrrrrrrrr");
+            Log.d(LOG_TAG, result.alt);
+            Log.d(LOG_TAG, "rrrrrrrrrrrrrrrrrrrrrrr");
+            textView.setText(result.title);
         }
     }
 
-    private String downloadUrl(String myurl) throws IOException {
+    private Book downloadUrl(String isbn) throws IOException {
         InputStream is = null;
-        // Only display the first 500 characters of the retrieved
-        // web page content.
-        int len = 50000;
 
         try {
-//            URL url = new URL("https://ps4market.herokuapp.come/get_more_content?latest_id=999999");
-            URL url = new URL("https://api.douban.com/v2/discussion/48381222");
+//            URL url = new URL("https://api.douban.com/v2/book/isbn/9787544713078");
+            Log.d(LOG_TAG, "isbn is "+ isbn);
+            String string_url = "https://api.douban.com/v2/book/isbn/" + isbn;
+            Log.d(LOG_TAG, "url is "+ string_url);
+            URL url = new URL(string_url);
             HttpURLConnection conn = (HttpURLConnection) url.openConnection();
             conn.setReadTimeout(10000 /* milliseconds */);
             conn.setConnectTimeout(15000 /* milliseconds */);
@@ -115,8 +118,7 @@ public class MainActivity extends Activity {
             is = conn.getInputStream();
 
             // Convert the InputStream into a string
-            String contentAsString = readIt(is);
-            return contentAsString;
+            return readIt(is);
 
             // Makes sure that the InputStream is closed after the app is
             // finished using it.
@@ -127,7 +129,13 @@ public class MainActivity extends Activity {
         }
     }
 
-    public String readIt(InputStream stream) throws IOException, UnsupportedEncodingException {
+    public class Book {
+        private String alt;
+        private String title;
+        private String subtitle;
+    }
+
+    public Book readIt(InputStream stream) throws IOException, UnsupportedEncodingException {
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
         byte [] buffer = new byte[1024];
         int len;
@@ -135,23 +143,13 @@ public class MainActivity extends Activity {
             baos.write(buffer, 0, len);
         }
         String jsonString = baos.toString();
-        JSONObject j = tojson(jsonString);
+        Log.d(LOG_TAG, "================================");
+        Log.d(LOG_TAG, jsonString);
+        Log.d(LOG_TAG, "================================");
+        Gson gson = new GsonBuilder().create();
+        Book book = gson.fromJson(jsonString, Book.class);
         baos.close();
-        return jsonString;
+        return book;
     }
 
-    public static JSONObject tojson(String json) {
-        try {
-            Log.d(LOG_TAG, "================================");
-            Log.d(LOG_TAG, json);
-            Log.d(LOG_TAG, "================================");
-            JSONObject jsonObj = new JSONObject(json);
-            Log.d(LOG_TAG, jsonObj.getString("updated"));
-            Log.d(LOG_TAG, jsonObj.getString("author"));
-            return jsonObj;
-        } catch (JSONException e) {
-            Log.d(LOG_TAG, "json error");
-            return null;
-        }
-    }
 }
